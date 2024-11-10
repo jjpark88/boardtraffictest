b9db2e5c6baa609c258814314b0fbabebf7e365e
jjpark88 <125102555+jjpark88@users.noreply.github.com>
2024-11-10 23:11:06 +0900

    댓글, 태그 기능 개발 (패키지명 일괄 변경)

--- src/main/java/com/jjpark/boardserver/exception/DuplicateIdException.java
+++
@@ -1,7 +0,0 @@
-package com.jjpark.boardServer.exception;
-
-public class DuplicateIdException extends RuntimeException {
-    public DuplicateIdException(String msg) {
-        super(msg);
-    }
-}
